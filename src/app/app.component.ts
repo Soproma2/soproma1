0169ca2b8f41cbeb28c9c2ce7774322540ca2a9d
Soproma2 <nikolozsopromadze@gmail.com>
2025-02-23 00:52:12 +0400

    add components
    add footer, header, homepage components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
